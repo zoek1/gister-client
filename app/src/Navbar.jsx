f2999d5b90a52e8a827024fdb4a2bc6d49ddd8fc
Monica <moonniy.2012@gmail.com>
2020-08-13 14:44:14 -0500

    Cambios de estilos del Navbar

--- app/src/Navbar.jsx
+++ app/src/Navbar.jsx
@@ -1,27 +1,68 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  display: grid;
+  grid-template-columns: 180px auto 400px;
+  background-color: #222222;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  justify-content: initial;
+`;
+
+const ContentImage = styled.a`
+  margin: auto;
+  text-decoration: none;
+`;
+
+const Imagen = styled.img`
+  width: 30;
+  height: 30;
+`;
+
+const ContentList = styled.ul`
+  display: flex;
+  justify-content: initial;
+`;
+
+const List = styled.li`
+  list-style: none;
+  margin: 0 20px;
+`;
+
+const Name = styled.a`
+  text-decoration: none;
+  color: #fafafa;
+`;
+
+const Search = styled.form`
+  margin: auto;
+`;
 
 function Navbar() {
   return (
-    <nav className="Navbar">
-      <a className="" href="#">
-        <img src="/" width="30" height="30" class="" alt=""/>Gister
-      </a>
-      <div className="" id="">
-        <ul className="">
-          <li className="">
-            <a className="" href="#">All gists</a>
-          </li>
-          <li className="">
-            <a className="" href="#">My gists</a>
-          </li>
-        </ul>
-      </div>
-      <form className="">
+    <Nav>
+      <ContentImage href="/">
+        <Imagen src="/" />
+      </ContentImage>
+      <Menu>
+        <ContentList>
+          <List>
+            <Name href="/">All gists</Name>
+          </List>
+          <List>
+            <Name className="" href="/">My gists</Name>
+          </List>
+        </ContentList>
+      </Menu>
+      <Search>
         <input className="" type="search" placeholder="Search" aria-label="Search"/>
         <button className="" type="submit">Search</button>
-      </form>
+      </Search>
 
-    </nav>
+    </Nav>
   );
 }
 
